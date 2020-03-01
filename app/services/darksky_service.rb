1950f6a6bf6868a67a5e48bfd8071cc993173745
Rachel Lew <rlew07@gmail.com>
2020-03-01 13:32:41 -0700

    Move API call into private method

--- app/services/darksky_service.rb
+++ app/services/darksky_service.rb
@@ -1,11 +1,15 @@
 class DarkskyService
   def forecast_by_coordinates(lat, long)
-    conn = Faraday.new(url: "https://api.darksky.net/forecast/") do |faraday|
-      faraday.adapter Faraday.default_adapter
-    end
-
     response = conn.get("#{ENV['DARKSKY_API_KEY']}/#{lat},#{long}")
 
     parsed = JSON.parse(response.body)
+  end
+
+  private
+
+  def conn
+    conn = Faraday.new(url: "https://api.darksky.net/forecast/") do |faraday|
+      faraday.adapter Faraday.default_adapter
+    end
   end
 end
