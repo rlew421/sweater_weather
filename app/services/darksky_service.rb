ef51f450e339195418b4f98a573f3c892d11e6ab
Rachel Lew <rlew07@gmail.com>
2020-03-01 13:34:38 -0700

    Move getting and parsing of JSON into private method

--- app/services/darksky_service.rb
+++ app/services/darksky_service.rb
@@ -1,11 +1,14 @@
 class DarkskyService
   def forecast_by_coordinates(lat, long)
-    response = conn.get("#{ENV['DARKSKY_API_KEY']}/#{lat},#{long}")
-
-    parsed = JSON.parse(response.body)
+    get_json("#{ENV['DARKSKY_API_KEY']}/#{lat},#{long}")
   end
 
   private
+
+  def get_json(url)
+    response = conn.get(url)
+    JSON.parse(response.body)
+  end
 
   def conn
     conn = Faraday.new(url: "https://api.darksky.net/forecast/") do |faraday|
