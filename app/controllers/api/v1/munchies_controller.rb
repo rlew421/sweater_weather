3f16aa9f729b49e5b553e1d0e8f2079ef2c51dba
Rachel Lew <rlew07@gmail.com>
2020-03-02 10:19:19 -0700

    Add MunchiesController with show method

---
+++ app/controllers/api/v1/munchies_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::MunchiesController < ApplicationController
+  def show
+    munchies_facade = MunchiesFacade.new(params[:start], params[:end], params[:food])
+    munchies_results = munchies_facade.get_munchies_travel_results
+    serialized = MunchiesSerializer.new(munchies_results)
+    render json: serialized
+  end
+end
