bdddcd6181bff5f25ab424397eaded9a2cd64302
Rachel Lew <rlew07@gmail.com>
2020-03-02 09:37:26 -0700

    Add test that API call to GoogleDirections can return estimated travel time

--- spec/services/google_geocoding_service_spec.rb
+++ spec/services/google_geocoding_service_spec.rb
@@ -1,10 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe GoogleGeocodingService do
- it "returns coordinates" do
-   search = subject.coordinates_by_city("Denver")
+ # xit "returns coordinates" do
+ #   search = subject.coordinates_by_city("Denver")
+ #   expect(search).to be_a Hash
+ #   expect(search["results"][0]["geometry"]["location"]["lat"]).to eq(39.7392358)
+ #   expect(search["results"][0]["geometry"]["location"]["lng"]).to eq(-104.990251)
+ # end
+
+ it "returns estimated travel time" do
+   origin_coordinates = 39.7392358,-104.990251
+   destination_coordinates = 38.2544472,-104.6091409
+   search = subject.estimated_travel_time(origin_coordinates, destination_coordinates)
    expect(search).to be_a Hash
-   expect(search["results"][0]["geometry"]["location"]["lat"]).to eq(39.7392358)
-   expect(search["results"][0]["geometry"]["location"]["lng"]).to eq(-104.990251)
+   expect(search["text"]).to eq("1 hour 46 mins")
+   expect(search["value"]).to eq(6382)
  end
 end
