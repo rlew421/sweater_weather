a0e347d5b1788a66a09b0fcc5d9bdb91a9e8f1b3
Rachel Lew <rlew07@gmail.com>
2020-03-02 10:17:54 -0700

    Add request spec for estimated travel time

--- spec/requests/api/v1/forecasts_request_spec.rb
+++ spec/requests/api/v1/forecasts_request_spec.rb
@@ -1,13 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe "Forecast API" do
-  it "sends forecast for a city" do
-    city = "denver"
-    state = "co"
+  # it "sends forecast for a city" do
+  #   city = "denver"
+  #   state = "co"
+  #
+  #   get "/api/v1/forecast?location=#{city},#{state}"
+  #
+  #   forecast = JSON.parse(response.body)
+  #   expect(response).to be_successful
+  # end
 
-    get "/api/v1/forecast?location=#{city},#{state}"
+  it "sends estimated travel time between start and end location" do
+    origin = "denver,co"
+    destination = "pueblo,co"
+    food_type = "chinese"
 
-    forecast = JSON.parse(response.body)
+    get "/api/v1/munchies?start=#{origin}&end=#{destination}&food=#{food_type}"
+
+    result = JSON.parse(response.body)
     expect(response).to be_successful
   end
 end
