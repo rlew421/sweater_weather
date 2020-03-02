49594bd7cca603a61b97b21ac55c88c44953d2ab
Rachel Lew <rlew07@gmail.com>
2020-03-01 20:26:13 -0700

    Create methods for parsing through current and daily forecast data

--- app/poros/forecast.rb
+++ app/poros/forecast.rb
@@ -1,23 +1,35 @@
 class Forecast
   attr_reader :id,
-              :summary,
-              :temperature,
-              :apparent_temperature,
-              :humidity,
-              :visibility,
-              :uv_index,
-              :time,
-              :timezone
+              :timezone,
+              :currently,
+              :daily,
+              :hourly
 
   def initialize(forecast_data)
     @id = nil
-    @summary = forecast_data["currently"]["summary"]
-    @temperature = forecast_data["currently"]["temperature"]
-    @apparent_temperature = forecast_data["currently"]["apparentTemperature"]
-    @humidity = forecast_data["currently"]["humidity"]
-    @visibility = forecast_data["currently"]["visibility"]
-    @uv_index = forecast_data["currently"]["uvIndex"]
-    @time = forecast_data["currently"]["time"]
     @timezone = forecast_data["timezone"]
+    @currently = parse_current_forecast(forecast_data["currently"])
+    @daily = parse_daily_forecast(forecast_data["daily"])
+  end
+
+  def parse_current_forecast(current_forecast_data)
+    {
+      summary: current_forecast_data["summary"],
+      temperature: current_forecast_data["temperature"],
+      apparent_temperature: current_forecast_data["apparentTemperature"],
+      humidity: current_forecast_data["humidity"],
+      visibility: current_forecast_data["visibility"],
+      uv_index: current_forecast_data["uvIndex"],
+      time: current_forecast_data["time"]
+    }
+  end
+
+  def parse_daily_forecast(daily_forecast_data)
+    {
+      summary: daily_forecast_data["summary"],
+      temperature_high: daily_forecast_data["data"].first["temperatureHigh"],
+      temperature_low: daily_forecast_data["data"].first["temperatureLow"],
+      time: daily_forecast_data["data"].first["time"]
+    }
   end
 end
