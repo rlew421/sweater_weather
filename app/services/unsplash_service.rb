3e164abce00e8bbca041523ad91351837f1b0f08
Rachel Lew <rlew07@gmail.com>
2020-03-02 00:01:13 -0700

    Move API call, getting and parsing of JSON into private methods

--- app/services/unsplash_service.rb
+++ app/services/unsplash_service.rb
@@ -1,12 +1,19 @@
 class UnsplashService
   def image_by_location(city, state)
-    conn = Faraday.new(url: "https://api.unsplash.com/search") do |faraday|
+    get_json("/photos?query=#{city}&query=#{state}")
+  end
+
+  private
+
+  def get_json(url)
+    response = conn.get(url)
+    JSON.parse(response.body)
+  end
+
+  def conn
+    Faraday.new(url: "https://api.unsplash.com/search") do |faraday|
       faraday.headers["Authorization"] = "Client-ID #{ENV['UNSPLASH_API_KEY']}"
       faraday.adapter Faraday.default_adapter
     end
-
-    response = conn.get("/photos?query=#{city}&query=#{state}")
-
-    parsed = JSON.parse(response.body)
   end
 end
