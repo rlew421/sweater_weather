dcccb627b0cdc89405ecdbbb65451c104f5d3200
Rachel Lew <rlew07@gmail.com>
2020-03-02 10:19:04 -0700

    Comment out estimated_travel_time method

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -21,4 +21,17 @@ class ForecastFacade
     image_data = UnsplashService.new.image_by_location(get_geolocation.city, get_geolocation.state)
     forecast = Forecast.new(forecast_data, image_data)
   end
+
+  # def get_estimated_travel_time(origin_location, destination_location)
+  #   service = GoogleGeocodingService.new
+  #   origin_coordinates = service.coordinates_by_city(origin_location)
+  #   directions_connection = Faraday.new(url: "https://maps.googleapis.com/maps/api/directions/json") do |faraday|
+  #     faraday.adapter Faraday.default_adapter
+  #   end
+  #
+  #   response = directions_connection.get("?origin=39.7392358,-104.990251&destination=38.2544472,-104.6091409&key=#{ENV['GOOGLE_GEOCODING_API_KEY']}")
+  #
+  #   results = JSON.parse(response.body)
+  #   final = results["routes"][0]["legs"][0]["duration"]
+  # end
 end
