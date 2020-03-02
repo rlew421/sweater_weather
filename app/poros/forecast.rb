a1d636b54351313ea5af6f523554a9c5a426dde4
Rachel Lew <rlew07@gmail.com>
2020-03-01 21:23:10 -0700

    Add parse_hourly_forecast method

--- app/poros/forecast.rb
+++ app/poros/forecast.rb
@@ -10,11 +10,13 @@ class Forecast
     @timezone = forecast_data["timezone"]
     @currently = parse_current_forecast(forecast_data["currently"])
     @daily = parse_daily_forecast(forecast_data["daily"])
+    @hourly = parse_hourly_forecast(forecast_data["hourly"])
   end
 
   def parse_current_forecast(current_forecast_data)
     {
       summary: current_forecast_data["summary"],
+      icon: current_forecast_data["icon"],
       temperature: current_forecast_data["temperature"],
       apparent_temperature: current_forecast_data["apparentTemperature"],
       humidity: current_forecast_data["humidity"],
@@ -27,9 +29,20 @@ class Forecast
   def parse_daily_forecast(daily_forecast_data)
     {
       summary: daily_forecast_data["summary"],
+      icon: daily_forecast_data["icon"],
       temperature_high: daily_forecast_data["data"].first["temperatureHigh"],
       temperature_low: daily_forecast_data["data"].first["temperatureLow"],
       time: daily_forecast_data["data"].first["time"]
+    }
+  end
+
+  def parse_hourly_forecast(hourly_forecast_data)
+    {
+      summary: hourly_forecast_data["summary"],
+      icon: hourly_forecast_data["icon"],
+      time: hourly_forecast_data["data"].first["time"],
+      temperature: hourly_forecast_data["data"].first["temperature"],
+      humidity: hourly_forecast_data["data"].first["humidity"]
     }
   end
 end
