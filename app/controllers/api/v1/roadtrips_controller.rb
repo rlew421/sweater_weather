fd9b45ff7c4a571dcfe0b539cf6377225bb322e0
Rachel Lew <rlew07@gmail.com>
2020-03-04 00:01:36 -0700

    Add conditional so that a roadtrip request made without an API key returns an error

--- app/controllers/api/v1/roadtrips_controller.rb
+++ app/controllers/api/v1/roadtrips_controller.rb
@@ -1,14 +1,23 @@
 class Api::V1::RoadtripsController < ApplicationController
   def create
-    roadtrip_facade = RoadtripFacade.new(roadtrip_params)
-    roadtrip = roadtrip_facade.get_roadtrip
-    serialized = RoadtripSerializer.new(roadtrip)
-    render json: serialized
+    if valid_user?
+      roadtrip_facade = RoadtripFacade.new(roadtrip_params)
+      roadtrip = roadtrip_facade.get_roadtrip
+      serialized = RoadtripSerializer.new(roadtrip)
+      render json: serialized
+    else
+      render json: { error: unauthorized }, status: 401
+    end
   end
 
   private
 
   def roadtrip_params
     params.permit(:origin, :destination)
+  end
+
+  def valid_user?
+    user = User.find_by(auth_token: params[:api_key])
+    user != nil
   end
 end
