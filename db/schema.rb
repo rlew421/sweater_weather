b6bdedc2d942f1d543005f6c8994119599e237c1
Rachel Lew <rlew07@gmail.com>
2020-03-03 18:45:34 -0700

    Add email and password_digest columns to Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_03_190921) do
+ActiveRecord::Schema.define(version: 2020_03_04_014405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2020_03_03_190921) do
     t.string "api_key"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
+    t.string "password_digest"
   end
 
 end
