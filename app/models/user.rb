04c0456c0cd36cbe85681842027e0f3295d42124
Rachel Lew <rlew07@gmail.com>
2020-03-03 21:11:27 -0700

    Remove unnecessary method to generate api key

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,7 @@
 class User < ApplicationRecord
-  validates_presence_of :email, :password_confirmation
+  validates_presence_of :email, :password_digest, :password_confirmation
   validates_uniqueness_of :email
+
+  has_secure_token :auth_token
   has_secure_password
-  has_secure_token :api_key
-
-  before_create do |user|
-    user.api_key = user.generate_api_key
-  end
-
-  def generate_api_key
-    loop do
-      token = SecureRandom.base64.tr('+/=', 'Qrt')
-      break token unless User.exists?(api_key: token)
-    end
-  end
 end
