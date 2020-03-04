2eb678790700e81a33ec834d6f3311fbcddff955
Rachel Lew <rlew07@gmail.com>
2020-03-03 21:09:51 -0700

    Change test to test that api key was properly assigned to a registered user

--- spec/requests/api/v1/user_registration_spec.rb
+++ spec/requests/api/v1/user_registration_spec.rb
@@ -4,17 +4,17 @@ RSpec.describe "User Registration API" do
   it "can receive full user registration data and send back an API key" do
     User.destroy_all
     body = {
-      "email": "rlew07@gmail.com",
-      "password": "password123",
-      "password_confirmation": "password123"
+      "email": "example@gmail.com",
+      "password": "password",
+      "password_confirmation": "password"
     }
 
     post '/api/v1/users', params: body
 
     response_api_key = JSON.parse(response.body)
-
     expect(response_api_key).to be_a Hash
-    expect(response_api_key).to have_key "api_key"
-    expect(response.status).to eq(201)
+    expect(response.status).to eq(200)
+    user = User.last
+    expect(response_api_key["data"]["attributes"]["api_key"]).to eq(user.auth_token)
   end
 end
