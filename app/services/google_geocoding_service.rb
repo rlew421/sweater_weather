0436733289aa5435149084fcf24081645e4549db
Rachel Lew <rlew07@gmail.com>
2020-03-03 23:31:52 -0700

    Add API call to GoogleDirections to service

--- app/services/google_geocoding_service.rb
+++ app/services/google_geocoding_service.rb
@@ -3,6 +3,14 @@ class GoogleGeocodingService
     get_json("?key=#{ENV['GOOGLE_GEOCODING_API_KEY']}&address=#{city}")
   end
 
+  def estimated_travel_time(origin_coordinates, destination_coordinates)
+    response = directions_conn.get("?origin=#{origin_coordinates.lat},#{origin_coordinates.lng}&destination=#{destination_coordinates.lat},#{destination_coordinates.lng}&key=#{ENV['GOOGLE_GEOCODING_API_KEY']}")
+
+    results = JSON.parse(response.body)
+
+    results["routes"][0]["legs"][0]["duration"]
+  end
+
   private
 
   def get_json(url)
@@ -12,6 +20,12 @@ class GoogleGeocodingService
 
   def conn
     Faraday.new(url: "https://maps.googleapis.com/maps/api/geocode/json") do |faraday|
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+
+  def directions_conn
+    Faraday.new(url: "https://maps.googleapis.com/maps/api/directions/json") do |faraday|
       faraday.adapter Faraday.default_adapter
     end
   end
