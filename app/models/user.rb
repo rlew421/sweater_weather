564b26663876169f56798262a821bc746bdb17f2
Rachel Lew <rlew07@gmail.com>
2020-03-03 18:45:45 -0700

    Add validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  validates_presence_of :email, :password_confirmation
+  validates_uniqueness_of :email
+  has_secure_password
+  has_secure_token :api_key
+
   before_create do |user|
     user.api_key = user.generate_api_key
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,5 +4,5 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of :email }
   it { should validate_uniqueness_of :email }
   it { should validate_presence_of :password }
-  it { should validate_uniqueness_of :password_confirmation }
+  it { should validate_presence_of :password_confirmation }
 end
